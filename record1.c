9b64d2a98c1b5e82a9227f4f233bd30d44a9e0e3
Logan Capaldo <logancapaldo@gmail.com>
2012-01-27 02:31:30 +0000

    Set and get for the ascii case

--- record1.c
+++ record1.c
@@ -2,17 +2,38 @@
 
 int32_t record1_get_codepoint(struct record1* s, size_t index)
 {
-  return 0;
+  if( s->len < index ) {
+    return -1;
+  }
+  int32_t val = s->str[index];
+  if( (val & 0x7f) == val ) {
+    return val;
+  }
+  return -1;
 }
 
 void record1_set_codepoint(struct record1* s, size_t index, int32_t value)
 {
-   
+  if( s->cap < index ) {
+    return;
+  }
+  if( index > s->len )
+  {
+     s->len = index + 1;
+  }
+  if( (value & 0x7f) == value )
+  {
+    s->str[index] = (char)value;
+  }
 }
 
 void record1_init(struct record1 *s, size_t cap)
 {
+  char *i, *e;
   s->str = malloc(cap);
+  i = s->str;
+  e = i + cap;
+  for(;i!=e;++i) *i = 0;
   s->aux = NULL;
   s->cap = cap;
   s->len = 0;
--- test/main.c
+++ test/main.c
@@ -4,5 +4,10 @@ int main()
 {
   struct record1 s;
   record1_init(&s, 5u);
+  record1_set_codepoint(&s, 0u, 'a');
+  int32_t c = record1_get_codepoint(&s, 0u);
+  if( c != 'a')
+    return 1;
+  
   return 0;
 }
