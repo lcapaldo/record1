5400d87f0d2cb5e5f1a2091a4cf6d8ef921d53f4
Logan Capaldo <logancapaldo@gmail.com>
2012-01-27 02:12:55 +0000

    Implemented init

--- record1.c
+++ record1.c
@@ -12,4 +12,8 @@ void record1_set_codepoint(struct record1* s, size_t index, int32_t value)
 
 void record1_init(struct record1 *s, size_t cap)
 {
+  s->str = malloc(cap);
+  s->aux = NULL;
+  s->cap = cap;
+  s->len = 0;
 }
---
+++ test/main.c
@@ -0,0 +1,8 @@
+#include "record1.h"
+
+int main()
+{
+  struct record1 s;
+  record1_init(&s, 5u);
+  return 0;
+}
